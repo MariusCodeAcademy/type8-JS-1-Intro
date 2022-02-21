eb64a288f5d1e97368735c550e4c91256d6160b3
Marius <marius@example.com>
2022-02-21 09:48:47 +0200

    init

---
+++ main.js
@@ -0,0 +1,37 @@
+/* console.log('Sveiki visi, Hello world');
+4 + 4;
+bloko komentaras
+console.log(2 + 2 + 5); */
+
+// kintamasis naujesnis visada naudosim
+// pasakom kompiuteriui issaugoti vieta atmintyje kur mes noresim saugoti ir naudoti reiksmes
+const x = 5; // number
+let y = 5;
+let w = '50'; // String
+let isMain = true; // Boolean 1
+let isYellow = false; // Boolean 0
+// if (isMain === true) {
+//   console.log('it is main');
+// }
+let user = {}; // object
+let myArray = []; // object/ array
+let isBlue = null;
+let notHere = undefined;
+let blur;
+// blur = 'sveiki';
+console.log(blur);
+// yra senesio tipo globalus kintamasis
+var z = 25;
+
+// x = 50; // ismeta klaida nes x yra contanta
+// y = 100;
+
+console.log('x = ' + x + ' y = ' + y);
+console.log('Sudetis');
+console.log(x + y);
+console.log('Atimtis');
+console.log(x - y);
+console.log('Daugyba');
+console.log(x * y);
+console.log('Dalyba');
+console.log(x / y);
---
+++ readme.md
@@ -0,0 +1,8 @@
+# intro points
+
+1. Introduction to Javascript and its possibilities;
+2. Brief history of Javascript (and what is Ecmascript)
+3. Variables
+4. Data Types
+5. Comments
+6. Math Operators
